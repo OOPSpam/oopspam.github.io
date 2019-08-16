1a0edacfb46c1d6803d47aca9622852657017b29
onaralili <onerelili@gmail.com>
2019-08-16 16:56:36 +0400

    added a new question to FAQ

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 (function() {
-    // var burger = document.querySelector('.nav-toggle');
     let burger = document.querySelector('#navbarBurger');
-    var menu = document.querySelector('#navigation-bar');
+    let menu = document.querySelector('#navigation-bar');
     burger.addEventListener('click', function() {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
