82e21867223f8310c7516cb15fee2518ebe6a298
onaralili <onerelili@gmail.com>
2019-11-01 20:46:16 +0400

    website optimzation

--- scripts/app.js
+++ scripts/app.js
@@ -6,17 +6,30 @@
         menu.classList.toggle('is-active');
     });
 
-    // Start of Tawk.to Script
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function () {
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/5dbbe236e4c2fa4b6bd976e1/default';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
-    // End of Tawk.to Script
+    window.addEventListener('scroll', () => {
+        setTimeout(()=> {
+            // Start of Tawk.to Script
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function () {
+                var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/5dbbe236e4c2fa4b6bd976e1/default';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+            // End of Tawk.to Script
+        },1000),
+            { once: true }
+    });
 
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+          m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+      ga('create', 'UA-103050221-1', 'auto');
+      ga('send', 'pageview');
 })();
 
