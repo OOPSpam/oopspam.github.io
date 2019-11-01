521bb5150a967c9337d9cae6776e6e9bd7c6d891
onaralili <onerelili@gmail.com>
2019-11-01 15:26:14 +0400

    alternative page implemented / live chat added

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,22 @@
-(function() {
+(function () {
     let burger = document.querySelector('#navbarBurger');
     let menu = document.querySelector('#navigation-bar');
-    burger.addEventListener('click', function() {
+    burger.addEventListener('click', function () {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
     });
+
+    // Start of Tawk.to Script
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/5dbbe236e4c2fa4b6bd976e1/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+    // End of Tawk.to Script
+
 })();
 
