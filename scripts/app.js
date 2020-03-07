3f2df7f3fd2a12f761c4a8766caef73dfb2d48ae
onaralili <onerelili@gmail.com>
2020-03-07 11:29:37 +0400

    navigation bar fixes

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,19 @@
 (function () {
     let burger = document.querySelector('#navbarBurger');
     let menu = document.querySelector('#navigation-bar');
+    let navbarItems = document.getElementsByClassName('navbar-item');
+
     burger.addEventListener('click', function () {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
     });
 
-    // window.addEventListener('scroll', () => {
-    //     setTimeout(()=> {
-    //         //  Start of Async Drift Code
-    //         "use strict";
-            
-    //         !function() {
-    //           var t = window.driftt = window.drift = window.driftt || [];
-    //           if (!t.init) {
-    //             if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-    //             t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-    //             t.factory = function(e) {
-    //               return function() {
-    //                 var n = Array.prototype.slice.call(arguments);
-    //                 return n.unshift(e), t.push(n), t;
-    //               };
-    //             }, t.methods.forEach(function(e) {
-    //               t[e] = t.factory(e);
-    //             }), t.load = function(t) {
-    //               var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-    //               o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-    //               var i = document.getElementsByTagName("script")[0];
-    //               i.parentNode.insertBefore(o, i);
-    //             };
-    //           }
-    //         }();
-    //         drift.SNIPPET_VERSION = '0.3.1';
-    //         drift.load('m57ine8eus26');
-    //         // End of Async Drift Code
-    //     },1000),
-    //         { once: true }
-    // });
+    for (let item of navbarItems) {
+      item.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+  }
 
     (function (i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
