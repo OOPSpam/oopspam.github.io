8e911c38fb1c4f9bcaeb4643329447aa23a66cf8
onaralili <onerelili@gmail.com>
2021-01-26 16:52:45 +0400

    removed google analytics

--- scripts/app.js
+++ scripts/app.js
@@ -14,14 +14,5 @@
         menu.classList.toggle('is-active');
     });
   }
-
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-          (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-          m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-      ga('create', 'UA-103050221-1', 'auto');
-      ga('send', 'pageview');
 })();
 
