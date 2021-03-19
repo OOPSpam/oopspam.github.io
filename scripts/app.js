7842dd4b83b152ea2721a6683fe8ef2e91545a93
onaralili <onerelili@gmail.com>
2021-03-19 22:13:31 +0400

    yearly/montly plans

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,26 @@
     let burger = document.querySelector('#navbarBurger');
     let menu = document.querySelector('#navigation-bar');
     let navbarItems = document.getElementsByClassName('navbar-item');
+    let planIntervalMonthly = document.querySelector('.plan-interval .monthly');
+    let planIntervalYearly = document.querySelector('.plan-interval .yearly');
+
+    planIntervalMonthly.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      planIntervalMonthly.classList.toggle("is-warning", "is-selected");
+      planIntervalYearly.classList.remove("is-warning", "is-selected");
+      updatePricing("month");
+    });
+
+    planIntervalYearly.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      planIntervalYearly.classList.toggle("is-warning", "is-selected");
+      planIntervalMonthly.classList.remove("is-warning", "is-selected");
+      updatePricing("year");
+    });
+
+
 
     burger.addEventListener('click', function () {
         burger.classList.toggle('is-active');
@@ -15,4 +35,15 @@
     });
   }
 })();
+
+function updatePricing(interval) {
+  if (interval === "month" & !document.querySelector('.yearly-price').classList.contains("is-hidden")) {
+    document.querySelectorAll('.yearly-price').forEach( f => f.classList.toggle("is-hidden"));
+    document.querySelectorAll('.monthly-price .plan-price-amount').forEach( f => f.classList.remove("has-text-grey", "has-text-strike"));
+  } else if (interval === "year" & document.querySelector('.yearly-price').classList.contains("is-hidden")) {
+    document.querySelectorAll('.yearly-price').forEach( f => f.classList.remove("is-hidden"));
+    document.querySelectorAll('.monthly-price .plan-price-amount').forEach( f => f.classList.add("has-text-strike", "has-text-grey"));
+
+  }
+}
 
