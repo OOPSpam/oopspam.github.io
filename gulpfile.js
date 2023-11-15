681edf499f84b143105a507e6ddfd00c98b02de1
onaralili <onerelili@gmail.com>
2023-11-15 15:02:37 -0600

    generate sitemap, new pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'); 
 var nunjucksRender = require('gulp-nunjucks-render');
+var sitemap = require('gulp-sitemap');
 
-gulp.task('nunjucks', function() {
+gulp.task('build', function() {
     // Gets .html and .nunjucks files in pages
     return gulp.src('pages/**/*.+(html|nunjucks)')
     // Renders template with nunjucks
@@ -11,3 +12,17 @@ gulp.task('nunjucks', function() {
     // output files in app folder
     .pipe(gulp.dest('./'))
   });
+
+  // Generate Sitemap.xml
+gulp.task('sitemap', function() {
+    var sources = [
+      '*.html',
+      './tools/*.html',
+      './compare/*.html'
+  ];
+  return gulp.src(sources, { base: './' })
+        .pipe(sitemap({
+                siteUrl: 'http://www.oopspam.com'
+        }))
+        .pipe(gulp.dest('./'))
+});
